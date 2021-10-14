185f875c304cea925eb490d2bc21cd0b319f8051
SmolenskiyAV <JoJo_666@list.ru>
2021-10-15 00:57:28 +0700

    Версия 0.3 Не работают методы добавления и удаления!

--- task.js
+++ task.js
@@ -75,5 +75,21 @@ class Library {
     addBook(book) {
        this.books.push(book);
     }
+
+    findBookBy(type, value) { //первый аргумент type - ключ, свойство объекта, в котором ищем нужное значение value
+       
+        const findResult = this.books.filter((book) => book[type] === value); // поиск в массиве books[]. Критерий поиска: искомое значение value в свойстве type одного из элементов массива. Результат - объект-массив.
+        if (findResult.length === 0) return null; // если ничиего не найдено, выводим null
+        return findResult.name; // выводим имя найденной книги   
+    }
+
+    giveBookByName(bookName) {
+
+        const giveResult = this.books.filter((book) => book[bookName] === this.books.name); // поиск в массиве books[]. Критерий поиска: книга(элемент массива books[], с именем bookName)
+        const indexResult = this.books.findIndex(giveResult[bookName]); //находим индекс элемента(книги), которая указана в запросе
+        if (giveResult.length === 0) return null; // если ничиего не найдено, выводим null
+        if (indexResult >= 0) delete this.books[indexResult]; // удаление найденной книги
+        return giveResult; // вывод (возвращение) найденной книги (массив)
+    }
 };
 
