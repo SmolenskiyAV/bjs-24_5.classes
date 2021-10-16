8575a9bad38ed6de4fd16cb228515fbd5c5bbe92
SmolenskiyAV <JoJo_666@list.ru>
2021-10-16 09:36:19 +0700

    Версия 1.1 Не работают перебор свойств в классе и вычисление средней оценки!

--- task.js
+++ task.js
@@ -77,19 +77,72 @@ class Library {
     }
 
     findBookBy(type, value) { //первый аргумент type - ключ, свойство объекта, в котором ищем нужное значение value
-       
-        const findResult = this.books.filter((book) => book[type] === value); // поиск в массиве books[]. Критерий поиска: искомое значение value в свойстве type одного из элементов массива. Результат - объект-массив.
-        if (findResult.length === 0) return null; // если ничиего не найдено, выводим null
-        return findResult.name; // выводим имя найденной книги   
+        const findResult = this.books 
+        .find(book => book[type] === value) // поиск в массиве books[]. Критерий поиска: искомое значение value в свойстве type одного из элементов массива. Результат - объект-массив.
+        if (findResult === undefined) return null; // если ничиего не найдено, выводим null
+        return findResult; // выводим весь объект найденной книги
     }
 
     giveBookByName(bookName) {
-
-        const giveResult = this.books.filter((book) => book[bookName] === this.books.name); // поиск в массиве books[]. Критерий поиска: книга(элемент массива books[], с именем bookName)
-        const indexResult = this.books.findIndex(giveResult[bookName]); //находим индекс элемента(книги), которая указана в запросе
-        if (giveResult.length === 0) return null; // если ничиего не найдено, выводим null
-        if (indexResult >= 0) delete this.books[indexResult]; // удаление найденной книги
+        
+        const giveResult = this.books 
+        .find((book) => book.name === bookName) // поиск в массиве books[]. Критерий поиска: книга(элемент массива books[], с именем bookName)
+        //.map(item => item !== undefined);
+        const indexResult = this.books.findIndex((book) => book.name === bookName); //находим индекс элемента(книги), которая указана в запросе... точнее - почему-то не находим... ????
+        if (giveResult === undefined) return null; // если ничиего не найдено, выводим null
+        else this.books.splice(indexResult, 1); // удаление найденной книги
         return giveResult; // вывод (возвращение) найденной книги (массив)
     }
+};
+
+// ################################ TASK 3 ####################################################
+
+class Student {
+
+constructor (name, gender, age) {
+    this.name = name;
+    this.gender = gender;
+    this.age = age;
+}
+
+addMark(mark, subject) { 
+    if (mark <= 0 || mark > 5) return 'Ошибка, оценка должна быть числом от 1 до 5';
+    if (this[subject] === undefined) { // проверка наличия предметного массива с оценками
+      this[subject] = [];
+      this[subject].push(mark); // добавление первой оценки в пустой только что созданный предметный массив оценок
+    } else {
+      this[subject].push(mark);// добавление оценки в конец предметного массива оценок
+    }
+}
+
+getAverageBySubject(subject) { // получение среднего арифметического оценок предметного массива
+    if (this[subject] === undefined) return 'Несуществующий предмет';
+    const average = this[subject].reduce((acc, item, idx, arr) => {
+      acc += item;
+      if (idx === arr.length - 1) {
+        return acc / arr.length;
+      } else return acc;
+    })
+  
+  return average; // вывод(возврат) среднего арифметического по запросу .getAverage
+  }
+
+getAverage() {
+  let sumAverage = 0;
+  let subjectsCount = 0; // начальное значение счётчика - количества предметов
+    for (let prop in Student) { //перебор всех свойств класса Student
+      if ((prop !== this.name) || (prop !== this.gender) || (prop !== this.age)) sumAverage += this.getAverageBySubject(prop); // в вычислении среднего значения участвуют только свойства, описывающие учебные предметы
+      subjectsCount += 1;
+  }
+  return Number((sumAverage/subjectsCount).toFixed(2)); // вычисление средней оценки по всем предметам, преобразование в число с обрезкой до 2го знака
+
+}
+
+exclude(reason) {
+    if (this.excluded !== undefined) return 'Студент уже исключён ранее!'; 
+    this.excluded = reason;
+    return 'reason'
+  };
+
 };
 
